211520938c44f267584589a69b8fb55449081f89
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-01-26 23:50:03 +0530

    Learned `console.clear`

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,12 @@ window.onload = () => {
     url: 'https://reqres.in/api/users',
     type: 'GET',
     success(response) {
-      console.log({ response });
       console.assert(
         response.per_page === 3,
         '[Error]: Item number per page is not truthy',
       );
+      console.clear();
+      console.log({ response });
     },
   });
 };
