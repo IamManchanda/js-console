b113b12362abae76f4362c35274ce9df9f199916
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-01-26 23:46:55 +0530

    Learned `console.assert`

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,11 @@ window.onload = () => {
     url: 'https://reqres.in/api/users',
     type: 'GET',
     success(response) {
-      console.log(response);
+      console.log({ response });
+      console.assert(
+        response.per_page === 3,
+        '[Error]: Item number per page is not truthy',
+      );
     },
   });
 };
